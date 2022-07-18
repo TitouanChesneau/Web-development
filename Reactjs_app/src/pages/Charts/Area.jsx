83c2c49783340561207d5c7e3d8b9a56feb97b3c
Titouan Chesneau <titouan.chesneau@epitech.eu>
2022-07-18 17:13:43 +0200

    [EDIT] graphs with good dark legend color

--- Reactjs_app/src/pages/Charts/Area.jsx
+++ Reactjs_app/src/pages/Charts/Area.jsx
@@ -10,7 +10,7 @@ const Area = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Area" title="Inflation Rate in percentage" />
-      <ChartComponent id="area-chart" height="420px" primaryXAxis={areaPrimaryXAxis} primaryYAxis={areaPrimaryYAxis} chartArea={{ border: { width: 0 }}} tooltip={{ enable: true }} background={ currentMode === 'Dark' ? '#33373E' : '#fff'}>
+      <ChartComponent id="area-chart" height="420px" primaryXAxis={areaPrimaryXAxis} primaryYAxis={areaPrimaryYAxis} chartArea={{ border: { width: 0 }}} tooltip={{ enable: true }} background={ currentMode === 'Dark' ? '#33373E' : '#fff'} legendSettings={{ background: (currentMode === 'Dark') ? '#011c20' : '#fff' }}>
         <Inject services={[DateTime, Legend, SplineAreaSeries]} />
         <SeriesCollectionDirective>
           {areaCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
